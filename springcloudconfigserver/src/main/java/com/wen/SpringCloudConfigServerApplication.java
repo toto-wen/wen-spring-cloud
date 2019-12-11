f645266ec981b71bc121361023bf2ffe0f82d882
wenpd <pandong.wen@topplusvision.com>
2019-12-11 10:30:34 +0800

    feat:上传代码 reviewer:pd.wen

--- springcloudconfigserver/src/main/java/com/wen/SpringCloudConfigServerApplication.java
+++ springcloudconfigserver/src/main/java/com/wen/SpringCloudConfigServerApplication.java
@@ -2,11 +2,13 @@ package com.wen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @Desc: 配置中心启动类
  */
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
     public static void main(String[] args){
         SpringApplication.run(SpringCloudConfigServerApplication.class, args);
---
+++ wenauth/src/main/java/com/wen/WenAuthApplication.java
@@ -0,0 +1,14 @@
+package com.wen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by PC-007 on 2019/12/11.
+ */
+@SpringBootApplication
+public class WenAuthApplication {
+    public static void main(String[] args){
+        SpringApplication.run(WenAuthApplication.class, args);
+    }
+}
