8cc2d1a18d09a754f3a31c082b1f5860aa89a72f
wenpd <pandong.wen@topplusvision.com>
2019-12-17 18:15:02 +0800

    fix:添加eureka模块 reviewer:pd.wen

---
+++ weneureka/src/main/java/com/wen/WenEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.wen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by PC-007 on 2019/12/17.
+ */
+@SpringBootApplication
+public class WenEurekaApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(WenEurekaApplication.class, args);
+    }
+}
